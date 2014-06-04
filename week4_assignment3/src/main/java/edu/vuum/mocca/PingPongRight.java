77e00fbdec8d763e91ebbac4b29c4d9aa4214a49
Michael Napoleon <michael.napoleon@gmail.com>
2014-06-04 14:00:36 -0400

    started week 4 work

--- week4_assignment3/src/main/java/edu/vuum/mocca/PingPongRight.java
+++ week4_assignment3/src/main/java/edu/vuum/mocca/PingPongRight.java
@@ -50,6 +50,7 @@ public class PingPongRight {
          * iteration.
          */
         // TODO - You fill in here.
+        private String printString;
 
         /**
          * Two SimpleSemaphores use to alternate pings and pongs.  You
@@ -57,6 +58,8 @@ public class PingPongRight {
          * two data members.
          */
         // TODO - You fill in here.
+        SimpleSemaphore semaphoreOne;
+        SimpleSemaphore semaphoreTwo;
 
         /**
          * Constructor initializes the data member(s).
@@ -66,6 +69,10 @@ public class PingPongRight {
                                   SimpleSemaphore semaphoreTwo,
                                   int maxIterations) {
             // TODO - You fill in here.
+            this.printString = stringToPrint;
+            this.semaphoreOne = semaphoreOne;
+            this.semaphoreTwo = semaphoreTwo;
+            this.mMaxLoopIterations = maxIterations;
         }
 
         /**
@@ -80,6 +87,13 @@ public class PingPongRight {
              */
 
             // TODO - You fill in here.
+          for (int count = 1; count <= mMaxLoopIterations; ++count) {
+            acquire();
+            System.out.println(printString);
+
+            release();
+          }
+          mLatch.countDown();
         }
 
         /**
@@ -87,6 +101,7 @@ public class PingPongRight {
          */
         void acquire() {
             // TODO fill in here
+            semaphoreOne.acquireUninterruptibly();
         }
 
         /**
@@ -94,6 +109,7 @@ public class PingPongRight {
          */
         void release() {
             // TODO fill in here
+            semaphoreTwo.release();
         }
     }
 
@@ -108,15 +124,15 @@ public class PingPongRight {
 
         // TODO initialize this by replacing null with the appropriate
         // constructor call.
-        mLatch = null;
+        mLatch = new CountDownLatch(2);
 
         // Create the ping and pong SimpleSemaphores that control
         // alternation between threads.
 
         // TODO - You fill in here, make pingSema start out unlocked.
-        SimpleSemaphore pingSema = null;
+        SimpleSemaphore pingSema = new SimpleSemaphore(1, false);
         // TODO - You fill in here, make pongSema start out locked.
-        SimpleSemaphore pongSema = null;
+        SimpleSemaphore pongSema = new SimpleSemaphore(0, false);
 
         System.out.println(startString);
 
@@ -125,19 +141,27 @@ public class PingPongRight {
         PlayPingPongThread ping = new PlayPingPongThread(/*
                                                           * TODO - You fill in
                                                           * here
-                                                          */);
+                                                          */
+                                                          pingString,pingSema, pongSema, maxIterations);
         PlayPingPongThread pong = new PlayPingPongThread(/*
                                                           * TODO - You fill in
                                                           * here
-                                                          */);
+                                                          */
+                                                          pongString, pongSema, pingSema, maxIterations);
 
         // TODO - Initiate the ping and pong threads, which will call
         // the run() hook method.
+        ping.start();
+        pong.start();
+
 
         // TODO - replace the following line with a barrier
         // synchronizer call to mLatch that waits for both threads to
         // finish.
-        throw new java.lang.InterruptedException();
+        try {
+          mLatch.await();
+        } catch(java.lang.InterruptedException e) {
+        }
 
         System.out.println(finishString);
     }
